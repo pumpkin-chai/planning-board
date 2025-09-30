058d7a04d9dc799f6165846a043abb88545f3a75
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-09-30 11:01:03 -0700

    Add placeholder home page

---
+++ app/home/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Home</h1>
+      <Label>This is a label</Label>
+      <Button>Click me</Button>
+    </main>
+  );
+}
