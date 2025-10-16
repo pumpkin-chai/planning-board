20e9bbebdb1191e619d900ec64accb805c2df232
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-10-16 11:14:02 -0700

    Use client component for group list

---
+++ app/planner/people/group-manager.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { ChevronRight, Plus, UserRoundSearch } from "lucide-react";
+import { useState } from "react";
+
+type Group = { id: number; name: string };
+
+export function GroupManager() {
+  const [groups, setGroups] = useState<Group[]>([
+    { id: 1, name: "Family" },
+    { id: 2, name: "Friends" },
+    { id: 3, name: "Work" },
+    { id: 4, name: "Gym Buddies" },
+    { id: 5, name: "Book Club" },
+  ]);
+
+  const handleNewGroup = () => {
+    console.log("New Group");
+  };
+
+  const handleLeaveGroup = (id: number) => {
+    setGroups(groups.filter((group) => group.id !== id));
+  };
+
+  return (
+    <div>
+      <div className="mb-6 flex items-stretch gap-4">
+        <CreateGroupButton onClickAction={handleNewGroup} />
+        <Button variant="outline">
+          <UserRoundSearch />
+          Join Group
+        </Button>
+      </div>
+      <GroupList groups={groups} onLeaveGroup={handleLeaveGroup} />
+    </div>
+  );
+}
+
+export function CreateGroupButton({
+  onClickAction,
+}: {
+  onClickAction: () => void;
+}) {
+  return (
+    <Button variant="default" onClick={onClickAction}>
+      <Plus />
+      Create Group
+    </Button>
+  );
+}
+
+function GroupList({
+  groups,
+  onLeaveGroup,
+}: {
+  groups: Group[];
+  onLeaveGroup: (id: number) => void;
+}) {
+  return (
+    <ul>
+      {groups.map((group) => (
+        <li key={group.id} className="mb-4 last:mb-0">
+          <GroupCard group={group} onLeaveGroup={onLeaveGroup} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function GroupCard({
+  group,
+  onLeaveGroup,
+}: {
+  group: Group;
+  onLeaveGroup: (id: number) => void;
+}) {
+  return (
+    <div className="border border-border p-6 rounded-lg cursor-pointer flex items-center justify-between gap-4 shadow-sm hover:shadow-md transition-shadow">
+      <div>
+        <h3 className="text-xl font-semibold">{group.name}</h3>
+        <span className="text-sm text-muted-foreground">5 members</span>
+      </div>
+      <div className="flex items-center">
+        <div className="flex items-center gap-4">
+          <Button variant="outline">Invite Members</Button>
+          <Button variant="default" onClick={() => onLeaveGroup(group.id)}>
+            Leave
+          </Button>
+        </div>
+        <ChevronRight className="ml-4" />
+      </div>
+    </div>
+  );
+}
--- app/planner/people/page.tsx
+++ app/planner/people/page.tsx
@@ -1,63 +1,35 @@
-import { Button } from "@/components/ui/button";
-import { ChevronRight, Plus, UserRoundSearch } from "lucide-react";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
+import { GroupManager } from "./group-manager";
 
-export default function People() {
-  type Group = { id: number; name: string };
-  const groups: Group[] = [
-    { id: 1, name: "Family" },
-    { id: 2, name: "Friends" },
-    { id: 3, name: "Work" },
-    { id: 4, name: "Gym Buddies" },
-    { id: 5, name: "Book Club" },
-  ];
+export default async function People() {
+  const supabase = await createClient();
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+  if (error || !user) {
+    redirect("/auth/login");
+  }
+
+  console.log(user);
+
+  const { data } = await supabase
+    .from("Memberships")
+    .select(`group_id`)
+    .eq("user_id", user.id);
+
+  console.log(data);
 
   return (
     <div className="px-8 py-3 w-full">
       <h1 className="self-start text-5xl font-bold mb-8">People</h1>
       <section>
         <h2 className="text-2xl mb-4">Your Groups</h2>
-        <div className="mb-6 flex items-stretch gap-4">
-          <Button variant="default">
-            <Plus />
-            Create Group
-          </Button>
-          <Button variant="outline">
-            <UserRoundSearch />
-            Join Group
-          </Button>
-        </div>
-        <GroupList groups={groups} />
+        <GroupManager />
       </section>
     </div>
   );
 }
 
-function GroupList({ groups }: { groups: Group[] }) {
-  return (
-    <ul>
-      {groups.map((group) => (
-        <li key={group.id} className="mb-4 last:mb-0">
-          <GroupCard group={group} />
-        </li>
-      ))}
-    </ul>
-  );
-}
-
-function GroupCard({ group }: { group: Group }) {
-  return (
-    <div className="border border-border p-6 rounded-lg cursor-pointer flex items-center justify-between gap-4 shadow-sm hover:shadow-md transition-shadow">
-      <div>
-        <h3 className="text-xl font-semibold">{group.name}</h3>
-        <span className="text-sm text-muted-foreground">5 members</span>
-      </div>
-      <div className="flex items-center">
-        <div className="flex items-center gap-4">
-          <Button variant="outline">Invite Members</Button>
-          <Button variant="default">Leave</Button>
-        </div>
-        <ChevronRight className="ml-4" />
-      </div>
-    </div>
-  );
-}
