6441d6f0df96f173f8e41240804b970c38fc7766
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-10-19 23:34:00 -0700

    Add calendar component

---
+++ app/planner/calendar/[groupId]/event-calendar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
+
+export function EventCalendar() {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
+  return (
+    <Calendar
+      // mode="single"
+      // selected={date}
+      // onSelect={setDate}
+      className="rounded-md border shadow-sm w-full md:w-3/5 lg:w-1/2"
+      captionLayout="dropdown"
+    />
+  );
+}
--- app/planner/calendar/[groupId]/page.tsx
+++ app/planner/calendar/[groupId]/page.tsx
@@ -1,5 +1,6 @@
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
+import { EventCalendar } from "./event-calendar";
 
 export default async function CalendarPage({
   params,
@@ -17,9 +18,24 @@ export default async function CalendarPage({
     redirect("/auth/login");
   }
 
+  const { data } = await supabase
+    .from("Groups")
+    .select("name")
+    .eq("id", groupId)
+    .single();
+  console.log("Group Data:", data);
+
   return (
     <div className="px-8 py-3 w-full">
-      <h1 className="self-start text-5xl font-bold mb-8">Calendar Page for Group ID: {groupId}</h1>
+      <h1 className="self-start text-5xl font-bold mb-8">
+        {data ? data.name : "Calendar"}
+      </h1>
+      <section>
+        <h2 className="text-2xl mb-4">Calendar</h2>
+        <div className="flex justify-center p-8 bg-secondary">
+          <EventCalendar />
+        </div>
+      </section>
     </div>
   );
 }
