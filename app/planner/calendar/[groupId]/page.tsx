047049323503621bbcaff959752f03b6484b745a
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-10-20 00:11:21 -0700

    Add event retrieval and list view

--- app/planner/calendar/[groupId]/page.tsx
+++ app/planner/calendar/[groupId]/page.tsx
@@ -18,22 +18,53 @@ export default async function CalendarPage({
     redirect("/auth/login");
   }
 
-  const { data } = await supabase
+  const { data: groupData } = await supabase
     .from("Groups")
     .select("name")
     .eq("id", groupId)
     .single();
-  console.log("Group Data:", data);
+  console.log("Group Data:", groupData);
+
+  type Event = { id: number; name: string; datetime: Date };
+  const { data: eventData } = await supabase
+    .from("Events")
+    .select("id, name, datetime")
+    .eq("group_id", groupId);
+  console.log("Event Data:", eventData);
+  const events: Event[] = eventData
+    ? eventData.map((event) => {
+        return {
+          id: event.id,
+          name: event.name,
+          datetime: new Date(event.datetime),
+        };
+      })
+    : [];
 
   return (
     <div className="px-8 py-3 w-full">
       <h1 className="self-start text-5xl font-bold mb-8">
-        {data ? data.name : "Calendar"}
+        {groupData ? groupData.name : "Calendar"}
       </h1>
-      <section>
+
+      <section className="mb-32">
         <h2 className="text-2xl mb-4">Calendar</h2>
         <div className="flex justify-center p-8 bg-secondary">
           <EventCalendar />
+        </div>
+      </section>
+
+      <section className="mb-16">
+        <h2 className="text-2xl mb-4">List</h2>
+        <div className="p-8 bg-secondary">
+          <ul className="overflow-y-auto">
+            {events.map((event) => (
+              <li key={event.id}>
+                <strong>{event.datetime.toLocaleDateString()} </strong>
+                <em>{event.datetime.toLocaleTimeString()}</em> - {event.name}
+              </li>
+            ))}
+          </ul>
         </div>
       </section>
     </div>
