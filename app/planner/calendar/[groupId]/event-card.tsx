b808ec1ad10745e31879e956dd6b57ebe5d2f0c0
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-10-20 17:20:26 -0700

    Apply event card styles based on status

--- app/planner/calendar/[groupId]/event-card.tsx
+++ app/planner/calendar/[groupId]/event-card.tsx
@@ -1,8 +1,17 @@
 import { Event } from "./event-calendar";
 
 export function EventCard({ event }: { event: Event }) {
+  let statusStyle: string = "";
+  if (event.status === "proposed") {
+    statusStyle = "text-muted-foreground";
+  } else if (event.status === "canceled") {
+    statusStyle = "text-red-600";
+  }
+
   return (
-    <div className="p-4 border rounded-md flex items-center gap-4">
+    <div
+      className={"p-4 border rounded-md flex items-center gap-4 " + statusStyle}
+    >
       <div className="flex-1">
         <h3 className="font-semibold">{event.title}</h3>
         <p className="text-sm text-muted-foreground">
