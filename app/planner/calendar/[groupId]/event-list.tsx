afa8ad061f8fa6f444458a766ae104bbc87a6ae5
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-11-23 21:52:16 -0800

    Refresh page on delete

--- app/planner/calendar/[groupId]/event-list.tsx
+++ app/planner/calendar/[groupId]/event-list.tsx
@@ -19,13 +19,16 @@ import { DialogDescription, DialogTitle } from "@radix-ui/react-dialog";
 import { Button } from "@/components/ui/button";
 import { createClient } from "@/lib/supabase/client";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export function EventList({ events }: { events: Event[] }) {
+  const router = useRouter();
+
   return (
     <ul className="overflow-y-auto">
       {events.map((event) => (
         <li key={event.id} className="mb-2 sm:mb-3 last:mb-0">
-          <EventCard event={event} />
+          <EventCard event={event} onDelete={() => router.refresh()} />
         </li>
       ))}
     </ul>
