7e7650d592b8dd26c9a9d29a7ee6c8d811e5c8ef
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-11-25 11:05:47 -0800

    Move Event type definition to types.d.ts

--- app/planner/calendar/[groupId]/event-calendar.tsx
+++ app/planner/calendar/[groupId]/event-calendar.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import { EventProposalDialog } from "./event-proposal-dialog";
 import { Button } from "@/components/ui/button";
 import { EventList } from "./event-list";
+import { Event } from "@/lib/types";
 
 export type EventResult = {
   id: number;
@@ -14,18 +15,6 @@ export type EventResult = {
   endsAt?: string | null;
   status: string;
   creator: { username: string };
-  isAttending: boolean;
-  attendeeCount: number;
-};
-
-export type Event = {
-  id: number;
-  title: string;
-  desc: string;
-  startsAt: Date;
-  endsAt: Date | null;
-  status: string;
-  creator: { username: string; currentUser: boolean };
   isAttending: boolean;
   attendeeCount: number;
 };
--- lib/types.d.ts
+++ lib/types.d.ts
@@ -1,3 +1,15 @@
+export type Event = {
+  id: number;
+  title: string;
+  desc: string;
+  startsAt: Date;
+  endsAt: Date | null;
+  status: string;
+  creator: { username: string; currentUser: boolean };
+  isAttending: boolean;
+  attendeeCount: number;
+};
+
 export type Group = {
   id: number;
   name: string;
