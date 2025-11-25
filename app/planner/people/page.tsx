542c5eadc0e8f98f219a609e59e9eb5f34246610
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-11-25 11:59:28 -0800

    Move GroupList to components/

--- app/planner/people/page.tsx
+++ app/planner/people/page.tsx
@@ -1,8 +1,6 @@
-import { createClient } from "@/lib/supabase/server";
 import { GroupManagementButtons } from "@/components/group-management-buttons";
-import { UserGroupResult } from "@/lib/types";
-import { GroupItem } from "@/components/group-item";
 import { Suspense } from "react";
+import { GroupList } from "@/components/group-list";
 
 export default function People() {
   return (
@@ -18,28 +16,5 @@ export default function People() {
         </div>
       </section>
     </div>
-  );
-}
-
-async function GroupList() {
-  const supabase = await createClient();
-
-  const { data, error } = await supabase
-    .from("user_groups")
-    .select("id:group_id, name:group_name, memberCount:member_count, role")
-    .order("role")
-    .overrideTypes<UserGroupResult[]>();
-  if (error || !data) {
-    return <div>Error loading group list</div>;
-  }
-
-  return (
-    <ul>
-      {data.map((group) => (
-        <li key={group.id} className="mb-2 sm:mb-4 last:mb-0">
-          <GroupItem group={group} />
-        </li>
-      ))}
-    </ul>
   );
 }
