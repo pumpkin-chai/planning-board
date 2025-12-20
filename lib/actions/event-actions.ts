e6b6177512656df261f43233167cdf12b837c9eb
Kenji Longid <58013636+kenmalik@users.noreply.github.com>
2025-12-19 21:34:34 -0800

    Add event status change notification generation

--- lib/actions/event-actions.ts
+++ lib/actions/event-actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { Event, EventStatus } from "@/lib/types"
-import { createClient, requireUser } from "../supabase/server";
+import { requireUser } from "../supabase/server";
 
 export async function setEventStatus(event: Event, status: EventStatus) {
     const { supabase, user } = await requireUser();
@@ -17,7 +17,7 @@ export async function setEventStatus(event: Event, status: EventStatus) {
         return { data: null, error: `Failed to change event "${event.title}" status to ${status.toLowerCase()}. Please try again later.` };
     }
 
-    const { error: notificationError } = await supabase
+    const { data: notificationData, error: notificationError } = await supabase
         .from("notifications")
         .insert({
             created_by: user.id,
@@ -29,10 +29,33 @@ export async function setEventStatus(event: Event, status: EventStatus) {
                 to: status
             }
         })
+        .select("id")
+        .maybeSingle();
 
-    if (notificationError) {
-        console.error("Error creating notification for event status change:", event, status, notificationError)
+    if (!notificationData || notificationError) {
+        console.error("Error creating notification for event status change:", event, status, notificationError);
+        return { data, error: null };
     }
+
+    const { data: groupMembers, error: groupMemberError } = await supabase
+        .from("memberships")
+        .select("user_id")
+        .eq("group_id", event.groupId);
+
+    if (!groupMembers || groupMemberError) {
+        console.error("Error querying group members:", groupMemberError);
+        return { data, error: null };
+    }
+
+    const promises = groupMembers.map((groupMember) =>
+        supabase
+            .from("user_notifications")
+            .insert({
+                user_id: groupMember.user_id,
+                notification_id: notificationData.id
+            })
+    )
+    Promise.allSettled(promises).then(results => results.forEach((result) => console.log(result)));
 
     return { data, error: null };
 }
